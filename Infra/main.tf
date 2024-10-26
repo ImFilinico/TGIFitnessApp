637326fb6ededcb27ed6556a128f1fe88af45a66
Nicolas Mangilit <nico.mangilit@outlook.com>
2024-10-26 18:58:29 -0500

    init tf and create s3

---
+++ Infra/main.tf
@@ -0,0 +1,61 @@
+resource "aws_s3_bucket" "tgifitness-bucket"{
+    bucket ="tgifitness-bucket"
+
+    tags ={
+        project = "TGIFitness"
+    }
+}
+
+resource "aws_s3_bucket_ownership_controls" "tgiftiness_bucket_ownership" {
+  bucket = aws_s3_bucket.tgifitness-bucket.bucket
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tgifitness_bucket_public_access_block" {
+  bucket = aws_s3_bucket.tgifitness-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "tgifitness_bucket_versioning" {
+  bucket = aws_s3_bucket.tgifitness-bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.tgifitness-bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "tgifitness_bucket_policy" {
+  bucket = aws_s3_bucket.tgifitness-bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.tgifitness-bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+#####################################################################################
+

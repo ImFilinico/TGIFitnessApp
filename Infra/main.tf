8f79b9f6a116f05d5fdd065f7b338563bf004114
Nicolas Mangilit <nico.mangilit@outlook.com>
2024-10-27 00:04:03 -0500

    tf to provision cloudfront

--- Infra/main.tf
+++ Infra/main.tf
@@ -68,5 +68,58 @@ resource "aws_s3_bucket_policy" "tgifitness_bucket_policy" {
   })
 }
 
+locals {
+  s3_origin_id = "tgifitness-s3-origin"
+}
+
 #####################################################################################
 
+resource "aws_cloudfront_distribution" "tgifitness_distribution" {
+    origin {
+        domain_name = aws_s3_bucket.tgifitness-bucket.bucket_regional_domain_name
+        origin_id = local.s3_origin_id
+
+        s3_origin_config {
+          origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+        }
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+      allowed_methods = ["GET", "HEAD"]
+      cached_methods = ["GET", "HEAD"]
+      target_origin_id = local.s3_origin_id
+
+      forwarded_values {
+        query_string = false
+        cookies {
+          forward = "none"
+        }
+      }
+      viewer_protocol_policy = "redirect-to-https"
+      min_ttl = 0
+      default_ttl = 3600
+      max_ttl = 86400
+    }
+
+    price_class = "PriceClass_100"
+
+    viewer_certificate {
+      cloudfront_default_certificate = true
+    }
+
+    restrictions {
+      geo_restriction {
+        restriction_type = "whitelist"
+        locations = ["US", "CA"]
+      }
+    }
+
+    tags = {
+        project = "TGIFitness"
+    }
+
+}

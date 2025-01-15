e8b12f1092da62b8524c5cfcf36fdcc8f34f5474
Nicolas Mangilit <nico.mangilit@outlook.com>
2025-01-15 10:27:44 -0600

    barebones navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/home'
 import Calculator from './pages/Calculator/calculator'
 import Log from './pages/Log/log'
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div>
         <Routes>
           <Route path="/" element={<Home />} />

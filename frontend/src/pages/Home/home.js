93a94424f513c5419c8c858dff1d0f46ea29430d
Nicolas Mangilit <nico.mangilit@outlook.com>
2025-02-05 22:55:15 -0600

    kicking pipeline

--- frontend/src/pages/Home/home.js
+++ frontend/src/pages/Home/home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <h1>Welcome to TGIFitness' Fitness Tracker</h1>
       <p>Go visit TGIFitness <a href="https://tgifitness.net/?sca_ref=6476147.zrjEk8PPLJx">here!</a> </p>
-      <p>Your go-to app for fitness tracking!</p>
+      <p>Your go-to app for fitness tracking! This text is testing the pipeline</p>
     </div>
   );
 };
